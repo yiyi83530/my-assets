74178da7784e2c3e0c9e40c23580edf7351ca002
yiyi83530 <92963881+yiyi83530@users.noreply.github.com>
2026-06-15 02:30:37 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   basePath: isProd ? '/my-assets' : '',
   images: {
     unoptimized: true,
